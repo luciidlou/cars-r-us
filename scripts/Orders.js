2845280f8f234bf57084d536acb2cc70873bf67b
luciidlou <lewisale96@gmail.com>
2021-10-20 11:34:11 -0500

    almost done

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,9 +1,42 @@
-import { getOrders } from "./database.js"
+import { getColors, getInteriors, getOrders, getTechnologies, getWheels } from "./database.js"
+
+const colors = getColors()
+const interiors = getInteriors()
+const technologies = getTechnologies()
+const wheels = getWheels()
+
 
 
 const buildOrderListItem = (order) => {
+    const foundColor = colors.find (
+        color => {
+            return color.id === order.colorId
+        }
+    )
+    const foundInterior = interiors.find(
+        interior => {
+            return interior.id === order.interiorId
+        }
+    )
+    const foundTechnology = technologies.find(
+        technology => {
+            return technology.id === order.technologyId
+        }
+    )
+    const foundWheel = wheels.find(
+        wheel => {
+            return wheel.id === order.wheelId
+        }
+    )
+    const totalCost = foundColor.price + foundInterior.price + foundTechnology.price + foundWheel.price
+
+    const costString = totalCost.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+        })
+
     return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+        Order #${order.id} cost ${costString}
     </li>`
 }
 
